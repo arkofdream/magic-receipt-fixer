84d94b77fc1d586d72f35d87e762eca9bbe91c39
arkofdream <ekizekiz392@gmail.com>
2026-08-15 20:03:10 +0300

    Add files via upload

---
+++ src/lib/download.ts
@@ -0,0 +1,6 @@
+/**
+ * Windows Masaüstü Uygulaması İndirme Bağlantısı
+ * GitHub Releases üzerinden en güncel sürümün kurulum dosyasını doğrudan indirir.
+ */
+export const DESKTOP_DOWNLOAD_URL =
+  "https://github.com/arkofdream/magic-receipt-fixer/releases/latest/download/MagicReceiptSetup.exe";
